8297a8f29989648bdb24d0e30b7bc9ed4bd9b8f2
Rivin Jayawardana <127378095+IT22061652@users.noreply.github.com>
2025-03-06 11:13:48 +0530

    hello

--- src/main/java/com/lab3/helloworld/HelloworldApplication.java
+++ src/main/java/com/lab3/helloworld/HelloworldApplication.java
@@ -2,6 +2,9 @@ package com.lab3.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 
 @SpringBootApplication
 public class HelloworldApplication {
@@ -9,5 +12,12 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public  String rootEndpoint(){
+		String message = "Hello world";
+		return message;
+	}
 }
+
+
+
