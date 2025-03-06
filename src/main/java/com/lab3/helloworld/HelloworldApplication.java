de14597cd110512afbb71faf53f038f547d813a2
Rivin Jayawardana <127378095+IT22061652@users.noreply.github.com>
2025-03-06 20:37:33 +0530

    namepara

--- src/main/java/com/lab3/helloworld/HelloworldApplication.java
+++ src/main/java/com/lab3/helloworld/HelloworldApplication.java
@@ -3,6 +3,7 @@ package com.lab3.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -13,9 +14,10 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
-	  @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+	@GetMapping("/greet/{name}")
+    public String hello(  @PathVariable String name,
+	@RequestParam(value = "message", defaultValue = "Welcome to Spring Boot!") String message) {
+      return String.format("Hello %s! %s", name,message);
     }
 }
 
